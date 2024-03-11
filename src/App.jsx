1cab9895d59ed6cd92ef3eecee50b6fddde55dbb
km-saifullah <kmsaifullah16@gmail.com>
2024-03-11 18:30:47 +0600

    my-blog app setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import conf from "./conf/config";
+
+console.log(conf);
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
